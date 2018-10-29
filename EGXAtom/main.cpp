b048f006418b71058aa9ddc9600ceeb9453cee8f
Metex <elliot_grafil@ufoh.com>
2018-10-29 11:52:53 -0700

    Parse data complete

--- EGXAtom/main.cpp
+++ EGXAtom/main.cpp
@@ -123,6 +123,21 @@ struct Isotope
     {}
 };
 
+
+int StringToInt(std::string & str) {
+    if (!str.empty()) {
+        return std::stoi(str);
+    }
+    return -1;
+}
+
+double StringToDouble(std::string & str) {
+    if (!str.empty()) {
+        return std::stod(str);
+    }
+    return -1.0;
+}
+
 void PrintIsotopeInfo( const Isotope & isotope )
 {
     std::cout << "Symbol:" <<isotope.Symbol << " A:" << isotope.A << " Mass:" << isotope.AtomicMassInteger << " " << isotope.AtomicMassMantissa << std::endl;
@@ -142,40 +157,9 @@ void removeCharsFromString(std::string &str, char* charsToRemove) {
     }
 }
 
-bool ReadAtomicMass(std::string FilePath) {	
-    std::cout << "In fucntion readatomic mass" << std::endl;
-    //Buffers	
-    char buffer[MAX_CHARS_PER_LINE];
-    std::vector<std::string> columns; // Holds data in columns
-    
 
-  	std::ifstream file;
-	file.open( FilePath.c_str() ); // open a file
-    if (!file.good()) {
-        std::cout << "FILE NOT FOUND" << std::endl;
-        return 0; // exit if file not found
-    }
-    	
-    
-    
-    //Advance pass header:
-    for (int i = 1; i <= LINES_TILL_DATA; i++) {
-        file.getline(buffer, MAX_CHARS_PER_LINE);
-    }
-    std::cout << "done";
-    while (!file.eof())
-  	{
-  		
-        std::cout << "in";
-        //Clears columns data
-        columns.clear();
-        columns.reserve(24);
-
-  		file.getline( buffer, MAX_CHARS_PER_LINE );
-  		
-        //Turn into columns
-        std::string lineData = std::string(buffer);
-
+bool ParseLineData(std::string &lineData, std::vector<std::string> &columns) {
+    if (!lineData.empty()) {
         columns.emplace_back(lineData.substr(0, 1));
         columns.emplace_back(lineData.substr(1, 3));
         columns.emplace_back(lineData.substr(4, 5));
@@ -198,44 +182,85 @@ bool ReadAtomicMass(std::string FilePath) {
         columns.emplace_back(lineData.substr(99, 1));
         columns.emplace_back(lineData.substr(100, 12));
         columns.emplace_back(lineData.substr(112, 11));
-        
+
         //Sanatize data:
         for (auto it = columns.begin(); it != columns.end(); it++) {
             char removeCharecters[] = "*# ";
             removeCharsFromString(*it, removeCharecters);
         }
+        return true;
+    }
+    return false;
+}
 
-        //Generate Isotope from data
-        std::stod(columns[15]); // Issue with null string
-        std::stod(columns[16]);
-        std::stod(columns[20]);
-        std::stod(columns[21]);
-        IsotopeData.emplace_back(
+bool AddIsotope(std::vector<std::string> &columns) {
+    //Generate Isotope from data
+    IsotopeData.emplace_back(
         Isotope(
             columns[0],
-            std::stoi(columns[1]),
-            std::stoi(columns[2]),
-            std::stoi(columns[3]),
-            std::stoi(columns[4]),
+            StringToInt(columns[1]),
+            StringToInt(columns[2]),
+            StringToInt(columns[3]),
+            StringToInt(columns[4]),
             columns[5],
             columns[6],
             columns[7],
             columns[8],
-            std::stod(columns[9]),
-            std::stod(columns[10]),
-            std::stod(columns[11]),
-            std::stod(columns[12]),
+            StringToDouble(columns[9]),
+            StringToDouble(columns[10]),
+            StringToDouble(columns[11]),
+            StringToDouble(columns[12]),
             columns[13],
             columns[14],
-            std::stod(columns[15]),
-            std::stod(columns[16]),
+            StringToDouble(columns[15]),
+            StringToDouble(columns[16]),
             columns[17],
-            std::stoi(columns[18]),
+            StringToInt(columns[18]),
             columns[19],
-            std::stod(columns[20]),
-            std::stod(columns[21])
+            StringToDouble(columns[20]),
+            StringToDouble(columns[21])
         ));
-        PrintIsotopeInfo(IsotopeData.back());
+    //PrintIsotopeInfo(IsotopeData.back());
+    return true;
+}
+
+bool ReadAtomicMass(std::string FilePath) {	
+    std::cout << "In fucntion readatomic mass" << std::endl;
+    //Buffers	
+    char buffer[MAX_CHARS_PER_LINE];
+    std::vector<std::string> columns; // Holds data in columns
+    
+
+  	std::ifstream file;
+	file.open( FilePath.c_str() ); // open a file
+    if (!file.good()) {
+        std::cout << "FILE NOT FOUND" << std::endl;
+        return 0; // exit if file not found
+    }
+    	
+    
+    
+    //Advance pass header:
+    for (int i = 1; i <= LINES_TILL_DATA; i++) {
+        file.getline(buffer, MAX_CHARS_PER_LINE);
+    }
+
+    while (!file.eof())
+  	{
+
+        //Clears columns data
+        columns.clear();
+        columns.reserve(24);
+
+  		file.getline( buffer, MAX_CHARS_PER_LINE );
+  		
+        //Turn into columns
+        std::string lineData = std::string(buffer);
+        
+        if (ParseLineData(lineData, columns)) {
+            AddIsotope(columns);
+        }
+      
 	}
     PrintIsotopeInfo(IsotopeData[0] );
     std::cout << "Size of isotope database:" << IsotopeData.size() << std::endl;
